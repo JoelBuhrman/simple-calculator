48505643b2209e00605205225c9d68a178986dc7
Joel Buhrman <joel.buhrman@live.se>
2019-09-18 21:27:56 +0200

    Fix issue with extra 0s at start of input

--- src/components/InputContent.js
+++ src/components/InputContent.js
@@ -3,14 +3,15 @@ import React from 'react'
 const InputContent = ({ label, value, setValue, calculate }) => {
   const updateValue = ({ target }) => {
     const { value } = target
-    setValue(parseInt(value, 10))
+
+    setValue(Number(value))
     calculate()
   }
 
   return (
     <div>
       {label}:
-      <input type='number' value={value} onChange={updateValue} />
+      <input type='number' value={value.toString()} onChange={updateValue} />
     </div>
   )
 }
