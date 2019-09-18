5aa12195130bd83620bd6808048db0face1f81c5
Joel Buhrman <joel.buhrman@live.se>
2019-09-18 21:43:57 +0200

    Add mockStore and wrap with provider for our App.js test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import App from './App'
+
+const storeData = {
+  firstNumber: 0,
+  secondNumber: 0,
+  thirdNumber: 0,
+  result: 0,
+  method: 'SUM'
+}
+const mockStore = configureStore([])
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={mockStore(storeData)}>
+      <App />
+    </Provider>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
